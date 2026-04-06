452d7ed6dadbfd07f31b36a3047dd46621b16bb9
ANA JÚLIA FARIA DA SILVA <anajulia.faria@a.ucb.br>
2026-04-06 20:57:39 -0300

    resolução exercicio3 06/04

---
+++ atividade3/estoque.js
@@ -0,0 +1,19 @@
+//mantem a lista de produtos em memoria
+const produtos = [];
+
+function cadastrar_produto(nome, qtd){
+    let produto = {
+        nome: nome,
+        qtd: qtd
+    }
+    produtos.push(produto);
+}
+
+function listar_produtos(){
+    return produtos;
+}
+
+module.exports = {
+    cadastrar_produto,
+    listar_produtos
+}
---
+++ atividade3/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const estoque = require('./estoque');
+const app     = express();
+const PORT    = 8080;
+
+app.get('/', (req, res)=>{
+    let msg = "<h1>Rotas disponiveis</h1>";
+    msg += "<p>Para cadastrar produtos: /api/cadastrar/:nome/:qtd </p>";
+    msg += "<p>Para cadastrar produtos: /api/listar</p>";
+    res.send(msg);
+});
+
+app.get('/api/cadastrar/:nome/:qtd', (req, res)=>{
+    let nome = req.params.nome;
+    let qtd = req.params.qtd;
+    estoque.cadastrar_produto(nome, qtd);
+    res.send('produto cadastrado em sucesso');                    
+});
+
+app.get('/api/listar', (req, res)=>{
+    res.send(estoque.listar_produtos);                    
+});
+
+
+app.listen(PORT, ()=>{
+    console.log('app rodando na porta ' + PORT);
+});
